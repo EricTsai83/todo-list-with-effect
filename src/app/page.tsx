a1de85f9cc5b3f55500d345c5a02b52e3a8e7182
Eric Tsai <eric492718@gmail.com>
2025-09-08 19:15:02 +0800

    implement interruption

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,49 +9,53 @@ type Todo = {
   completed: boolean;
 };
 
-const getTodo = async (id: number): Promise<unknown> => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
+const getTodo = async (
+  id: number,
+  opt?: { signal?: AbortSignal },
+): Promise<unknown> => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+    signal: opt?.signal,
+  });
   return await res.json();
 };
 
-// const getTodos = async (ids: number[]) => {
-//   const chunkSize = 5;
-//   const todos: unknown[] = [];
-//   for (let i = 0; i < ids.length; i += chunkSize) {
-//     const chunk = ids.slice(i, i + chunkSize);
-//     const chunkTodos = await Promise.all(chunk.map(getTodo));
-//     todos.push(...chunkTodos);
-//   }
-//   return todos;
-// };
-
-const getTodos = (ids: number[], limit = 5) => {
+const getTodos = (
+  ids: number[],
+  opt?: { limit?: number; signal?: AbortSignal },
+) => {
+  const limit = opt?.limit ?? 5;
+  const controller = new AbortController();
   const remaining = ids
-    .slice(0, ids.length) // ids 的淺拷貝
-    .map((id, index) => [id, index] as const) // 例如輸入 [101, 102, 103] → [[101,0],[102,1],[103,2]]
-    .reverse(); // 為了方便後續用 pop() 拿出剩下的任務
-
-  const results: unknown[] = []; // 用來存放 fetch 回來的資料，index 會對應到原來的 ids index
+    .slice(0, ids.length)
+    .map((id, index) => [id, index] as const)
+    .reverse();
+  const results: unknown[] = [];
+  if (opt?.signal) {
+    opt.signal.addEventListener("abort", () => {
+      controller.abort();
+    });
+  }
   return new Promise<unknown[]>((resolve, reject) => {
-    // 起始並行請求
-    let pending = 0; //  記錄目前正在執行的請求數量
+    let pending = 0;
     for (let i = 0; i < limit; i++) {
-      fetchRemaining(); // 一開始會同時啟動 limit 個請求
+      fetchRemaining();
     }
-
     function fetchRemaining() {
       if (remaining.length > 0) {
-        const [remainingToFetchId, remainingToFetchIdx] = remaining.pop()!; // 拿出第一個任務，並從 remaining 中移除
-        pending++; // 正在執行的請求數量 + 1
-        getTodo(remainingToFetchId)
+        const [remainingToFetchId, remainingToFetchIdx] = remaining.pop()!;
+        pending++;
+        getTodo(remainingToFetchId, { signal: controller.signal })
           .then((res) => {
-            results[remainingToFetchIdx] = res; // 確保結果按照 ids 原始順序存放在 results。JavaScript 陣列允許「先放後面，再補前面」，即便 results[0] 還是 undefined，也不會影響後續再補上。
+            results[remainingToFetchIdx] = res;
             pending--;
             fetchRemaining();
           })
-          .catch((err) => reject(err)); // 如果有一個請求失敗，就 reject 整個 Promise
+          .catch((err) => {
+            controller.abort();
+            return reject(err);
+          });
       } else if (pending === 0) {
-        resolve(results); // 全部完成後 resolve(results)
+        resolve(results);
       }
     }
   });
